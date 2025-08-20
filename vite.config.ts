04c26d66b5fc2160e3b767d6e1932d1270e2cf12
alwdavis81 <105399154+alwdavis81@users.noreply.github.com>
2025-08-20 01:55:52 -0400

    fix: Correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/alwdavis81/anime-osuume', // <--- REPLACE THIS WITH YOUR REPOSITORY NAME
+  base: '/anime-osuume/', // Corrected base for GitHub Pages
   plugins: [react()],
 })
