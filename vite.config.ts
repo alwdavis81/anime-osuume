23e95e28660974a1393748e993763e6240a04462
alwdavis81 <105399154+alwdavis81@users.noreply.github.com>
2025-08-20 01:45:50 -0400

    feat: Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/alwdavis81/anime-osuume', // <--- REPLACE THIS WITH YOUR REPOSITORY NAME
   plugins: [react()],
 })
